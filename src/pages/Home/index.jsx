2c77ed019aede568a6f152c76c0d60de901ff75d
Lucas Silverio Marques <lucas.marques@C02YPELRLVCF.local>
2020-06-04 17:20:04 -0300

    teste

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,31 +1,25 @@
-import React from 'react'
-import './style.css'
+import React from "react";
+import "./style.css";
 
-import InitialButton from '../../components/Form/InitialButton'
-import TextArea from '../../components/Form/TextArea'
+import InitialButton from "../../components/Form/InitialButton";
+import TextArea from "../../components/Form/TextArea";
 
-  
-export default function Header(){
-   
+export default function Header() {
+  console.log("helo");
 
-    return (
-        <div className= "text">
-            <h1>
-                Qual o seu estilo?
-            </h1>
-            <p>
-            Você tem ideia de qual é seu estilo feito por afroempreendedores? 
-            Identificamos dentre os fornecedores do Clube da Preta alguns diversos estilos de moda. 
-            E conseguimos identificar 5 que são os mais comuns entre eles: Basico, Clássico, Casual, Esporte e Fashion.
-            Que tal participar do nosso quiz para saber qual mais combina com você? Vem, vamos descobrir juntos!
-            </p>   
-            <TextArea/>
-            <InitialButton/>         
-        </div>
-            
-        
-
-
-    )
-
-} 
+  return (
+    <div className="text">
+      <h1>Qual o seu estilo?</h1>
+      <p>
+        Você tem ideia de qual é seu estilo feito por afroempreendedores?
+        Identificamos dentre os fornecedores do Clube da Preta alguns diversos
+        estilos de moda. E conseguimos identificar 5 que são os mais comuns
+        entre eles: Basico, Clássico, Casual, Esporte e Fashion. Que tal
+        participar do nosso quiz para saber qual mais combina com você? Vem,
+        vamos descobrir juntos!
+      </p>
+      <TextArea />
+      <InitialButton />
+    </div>
+  );
+}
