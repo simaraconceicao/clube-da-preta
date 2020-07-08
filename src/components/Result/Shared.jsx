4d362519a93c9385aa69cc60305080eeeae8b90d
systemdo <lsilveriomarques@gmail.com>
2020-07-07 22:54:36 -0300

    preparando para producao

--- src/components/Result/Shared.jsx
+++ src/components/Result/Shared.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useContext, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
